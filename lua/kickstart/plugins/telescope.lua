7bfefe1356e022dd24a9faf6b6fbf425a1abc6c8
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2025-01-21 18:13:08 -0600

    Added Zoxide to telescope 🎉
    Added package jvgrootveld/telescope-zoxide and learned a ton about
    telescope!!!

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -27,6 +27,8 @@ return {
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
+      { 'jvgrootveld/telescope-zoxide' },
+
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
@@ -72,6 +74,7 @@ return {
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'zoxide')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
@@ -85,6 +88,8 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+      vim.keymap.set('n', '<leader>z', require('telescope').extensions.zoxide.list)
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
